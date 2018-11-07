25817ac931c2c4e5ff0a136ecd8c9e9397567c91
Aleksandr <oleksandrs.mageside@gmail.com>
2018-11-08 01:54:16 +0200

    create requirejs-config and make x-init with helps of alias to js module. new data init + new js file (norm way)

--- app/code/Geekhub/Lesson5/view/frontend/web/js/example.js
+++ app/code/Geekhub/Lesson5/view/frontend/web/js/example.js
@@ -1,7 +1,6 @@
 define([], function () {
     var mageJsComponent = function(config, node)
     {
-        alert("Your Js module is working");
         console.log(config);
         console.log(node);
     };
