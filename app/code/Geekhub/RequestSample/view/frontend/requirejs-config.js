36f69cfee4ce21e50548764e0af92b270249e3a2
Maksym Zapoorozhets <maksimzaporozhets@gmail.com>
2018-11-15 01:32:56 +0200

    Replaced the jquery.cookie component with the newer implementation

--- app/code/Geekhub/RequestSample/view/frontend/requirejs-config.js
+++ app/code/Geekhub/RequestSample/view/frontend/requirejs-config.js
@@ -2,7 +2,9 @@ var config = {
     map: {
         '*': {
             geekHub_requestSample: 'Geekhub_RequestSample/js/request-sample',
-            geekHub_validationAlert: 'Geekhub_RequestSample/js/validation-alert'
+            geekHub_validationAlert: 'Geekhub_RequestSample/js/validation-alert',
+            // overriding default cookie component
+            'jquery/jquery.cookie': 'Geekhub_RequestSample/js/jquery/jquery.cookie'
         }
     }
 };
