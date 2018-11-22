8e8c5e2220b3f54aff38c6a2ea95e10d477675e0
Dmitry Chabanenko <chdn6026@mail.ru>
2018-11-22 09:23:22 +0200

    Lesson 07: Knockout demo by Alexandr Soldat (meals list)

--- app/code/Geekhub/Lesson7/view/frontend/web/js/view/grid.js
+++ app/code/Geekhub/Lesson7/view/frontend/web/js/view/grid.js
@@ -6,19 +6,64 @@ define(
     ],
     function ($, ko, component) {
         "use strict";
+        function SeatReservation(name, initialMeal) {
+            var self = this;
+            self.name = name;
+            self.meal = ko.observable(initialMeal);
+            self.formattedPrice = ko.computed(function() {
+                var price = self.meal().price;
+                return price ? "$" + price.toFixed(2) : "None";
+            });
+        }
 
         return component.extend({
-            firstName: ko.observable('John'),
-            lastName: ko.observable('Doe'),
-            initialize: function() {
-                this._super();
-                this.fullName = ko.pureComputed(function () {
-                    return this.firstName() + ' ' + this.lastName();
-                }, this);
+            seats: ko.observableArray([]),
+            names: ko.observableArray(['Henry','Chirag']),
+            availableMeals : ko.observableArray([
+                { mealName: "Standard (sandwich)", price: 5 },
+                { mealName: "Premium (lobster)", price: 14.95 },
+                { mealName: "Ultimate (two lobster)", price: 25 }
+                ]),
+            addSeat : function() {
+                var seat = new SeatReservation ("",this.availableMeals()[0]);
+                this.seats.push(seat);
             },
+            removeSeat : function(parent,seat) {
+                parent.seats.remove(seat);
+
+            },
+
+            addMeal:function(meals){
+                for (var i in meals){
+                    this.availableMeals.push(meals[i]);
+                }
+
+            },
+
+            totalSurcharge : function() {
+                var total = 0;
+                for (var i = 0; i < this.seats().length; i++)
+                    total += this.seats()[i].meal().price;
+                return total;
+            },
+
+            initialize: function () {
+                this._super();
+                this._render();
+
+            },
+
+            _render:function(){
+                var self = this;
+                for (var i = 0; i < this.names().length; i++){
+                    // console.log(i)
+                    this.seats.push(new SeatReservation (this.names()[i],this.availableMeals()[0]));
+                }
+            },
+
             defaults: {
-                template: 'Geekhub_Lesson7/grid'
-            }
+                template: 'Geekhub_Lesson7/grid',
+            },
         });
     }
 );
