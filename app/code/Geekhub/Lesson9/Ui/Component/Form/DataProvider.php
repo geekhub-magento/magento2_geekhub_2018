6378745601c6bcb42b8897cec29c8e3999debd89
Victor Tkachenko <victort@mageside.com>
2018-12-02 14:56:49 +0200

    Lesson 9: UiComponent - Form

---
+++ app/code/Geekhub/Lesson9/Ui/Component/Form/DataProvider.php
@@ -0,0 +1,61 @@
+<?php
+namespace Geekhub\Lesson9\Ui\Component\Form;
+
+use Magento\Cron\Model\ResourceModel\Schedule\CollectionFactory;
+
+/**
+ * Class DataProvider
+ */
+class DataProvider extends \Magento\Ui\DataProvider\AbstractDataProvider
+{
+    /**
+     * @var \Magento\Cron\Model\ResourceModel\Schedule\Collection
+     */
+    protected $collection;
+
+    /**
+     * @var array
+     */
+    protected $loadedData;
+
+    /**
+     * Constructor
+     *
+     * @param string $name
+     * @param string $primaryFieldName
+     * @param string $requestFieldName
+     * @param CollectionFactory $blockCollectionFactory
+     * @param array $meta
+     * @param array $data
+     */
+    public function __construct(
+        $name,
+        $primaryFieldName,
+        $requestFieldName,
+        CollectionFactory $blockCollectionFactory,
+        array $meta = [],
+        array $data = []
+    ) {
+        $this->collection = $blockCollectionFactory->create();
+        parent::__construct($name, $primaryFieldName, $requestFieldName, $meta, $data);
+    }
+
+    /**
+     * Get data
+     *
+     * @return array
+     */
+    public function getData()
+    {
+        if (isset($this->loadedData)) {
+            return $this->loadedData;
+        }
+        $items = $this->collection->getItems();
+        /** @var \Magento\Cron\Model\Schedule $job */
+        foreach ($items as $job) {
+            $this->loadedData[$job->getId()] = $job->getData();
+        }
+
+        return $this->loadedData;
+    }
+}
