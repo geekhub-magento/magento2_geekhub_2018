86539546eb9937babf580256904fddede1dcae22
Aleksandr <oleksandrs.mageside@gmail.com>
2019-01-09 01:49:26 +0200

    add data from CSV file programmatically

--- app/code/Geekhub/RequestSample/Setup/UpgradeData.php
+++ app/code/Geekhub/RequestSample/Setup/UpgradeData.php
@@ -7,20 +7,40 @@ use Magento\Framework\Setup\ModuleDataSetupInterface;
 use Magento\Framework\Setup\UpgradeDataInterface;
 use Magento\Store\Model\Store;
 use Geekhub\RequestSample\Model\RequestSample;
+use Magento\Framework\Component\ComponentRegistrar;
+use \Magento\Framework\File\Csv;
 
 class UpgradeData implements UpgradeDataInterface
 {
     /**
-     * @var \Geekhub\RequestSample\Model\RequestSampleFactory $requestSampleFactory
+     * @var \Geekhub\RequestSample\Model\RequestSampleFactory
      */
     private $requestSampleFactory;
 
     /**
+     * @var Csv
+     */
+    private $csv;
+
+    /**
+     * @var ComponentRegistrar
+     */
+    private $componentRegistrar;
+
+    /**
      * UpgradeData constructor.
      * @param \Geekhub\RequestSample\Model\RequestSampleFactory $requestSampleFactory
+     * @param ComponentRegistrar $componentRegistrar
+     * @param Csv $csv
      */
-    public function __construct(\Geekhub\RequestSample\Model\RequestSampleFactory $requestSampleFactory)
+    public function __construct(
+        \Geekhub\RequestSample\Model\RequestSampleFactory $requestSampleFactory,
+        ComponentRegistrar $componentRegistrar,
+        Csv $csv
+    )
     {
+        $this->componentRegistrar = $componentRegistrar;
+        $this->csv = $csv;
         $this->requestSampleFactory = $requestSampleFactory;
     }
 
@@ -29,6 +49,7 @@ class UpgradeData implements UpgradeDataInterface
      */
     public function upgrade(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
     {
+        $setup->startSetup();
         if (version_compare($context->getVersion(), '1.0.2', '<')) {
             for ($i = 1; $i <= 5; $i++) {
                 /** @var RequestSample $requestSample */
@@ -44,5 +65,75 @@ class UpgradeData implements UpgradeDataInterface
                 $requestSample->save();
             }
         }
+
+        if (version_compare($context->getVersion(), '1.0.3') < 0) {
+
+            $this->updateDataForRequestSample($setup, 'import_data.csv');
+
+        }
+        $setup->endSetup();
+    }
+
+    /**
+     * @param ModuleDataSetupInterface $setup
+     * @param $fileName
+     * @throws \Exception
+     */
+    public function updateDataForRequestSample(ModuleDataSetupInterface $setup, $fileName)
+    {
+        $tableName = $setup->getTable('geekhub_request_sample');
+        $file_path = $this->getPathToCsvMagentoAtdec($fileName);
+        $csvData = $this->csv->getData($file_path);
+
+        if ($setup->getConnection()->isTableExists($tableName) == true) {
+            foreach ($csvData as $row => $data) {
+                if (count($data) == 9) {
+                    $res = $this->getCsvData($data);
+                    $setup->getConnection()->insertOnDuplicate(
+                        $tableName, $res,
+                        [
+                            'name',
+                            'email',
+                            'phone',
+                            'product_name',
+                            'sku',
+                            'request',
+                            'created_at',
+                            'status',
+                            'store_id',
+                        ]);
+                }
+            }
+        }
+    }
+
+    /**
+     * @param $fileName
+     * @return string
+     */
+    private function getPathToCsvMagentoAtdec($fileName)
+    {
+        return $this->componentRegistrar->getPath(ComponentRegistrar::MODULE, 'Geekhub_RequestSample') .
+            DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . $fileName;
+    }
+
+    /**
+     * @param $data
+     * @return array
+     */
+    private function getCsvData($data)
+    {
+        return [
+            'name' => $data[0],
+            'email' => $data[1],
+            'phone' => $data[2],
+            'product_name' => $data[3],
+            'sku' => $data[4],
+            'request' => $data[5],
+            'created_at' => $data[6],
+            'status' => $data[7],
+            'store_id' => $data[8],
+        ];
     }
 }
+
